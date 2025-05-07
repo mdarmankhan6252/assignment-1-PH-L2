2a7a697f82e74bf6572ebc769d9947deeda69b44
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-05-08 00:22:45 +0600

    I have completed 6 problems

--- index.js
+++ index.js
@@ -18,4 +18,54 @@ const books = [
     { title: "Book B", rating: 3.2 },
     { title: "Book C", rating: 3.0 }
 ];
-console.log(filterByRating(books));
+// console.log(filterByRating(books));
+// -----------------------------------
+function concatenateArrays(...arrays) {
+    return [].concat(...arrays);
+}
+// console.log(concatenateArrays(['Arman', 'Yeassin'], ["JS", "TS"], ["C++"], ['Tomi', 'Ami']));
+//---------------------------------
+class Vehicle {
+    constructor(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    getInfo() {
+        console.log(`Make: ${this.make}, Year: ${this.year}`);
+    }
+}
+class Car extends Vehicle {
+    constructor(make, year, model) {
+        super(make, year);
+        this.model = model;
+    }
+    getModel() {
+        console.log(`Model: ${this.model}`);
+    }
+}
+const myCar = new Car('Toyota', 2024, 'Corolla');
+myCar.getInfo();
+myCar.getModel();
+// ----------------------------
+function processValue(value) {
+    if (typeof value === 'string') {
+        return value.length;
+    }
+    else {
+        return value * 2;
+    }
+}
+function getMostExpensiveProduct(products) {
+    if (products.length === 0) {
+        return null;
+    }
+    return products.reduce((maxValue, currentValue) => {
+        return currentValue.price > maxValue.price ? currentValue : maxValue;
+    });
+}
+const products = [
+    { name: "Pen", price: 100 },
+    { name: "Notebook", price: 25 },
+    { name: "Bag", price: 50 }
+];
+console.log(getMostExpensiveProduct(products));
--- index.ts
+++ index.ts
@@ -23,7 +23,101 @@ const books = [
    { title: "Book C", rating: 3.0 }
 ];
 
-console.log(filterByRating(books));
+// console.log(filterByRating(books));
+
+
+// -----------------------------------
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+   return ([] as T[]).concat(...arrays)
+}
+
+// console.log(concatenateArrays(['Arman', 'Yeassin'], ["JS", "TS"], ["C++"], ['Tomi', 'Ami']));
+
+
+//---------------------------------
+
+class Vehicle {
+   private make: string;
+   private year: number;
+   constructor(make: string, year: number) {
+      this.make = make;
+      this.year = year;
+   }
+
+   getInfo() {
+      console.log(`Make: ${this.make}, Year: ${this.year}`)
+   }
+}
+
+class Car extends Vehicle {
+   model: string;
+
+   constructor(make: string, year: number, model: string) {
+      super(make, year)
+      this.model = model;
+   }
+
+   getModel() {
+      console.log(`Model: ${this.model}`)
+   }
+}
+
+
+const myCar = new Car('Toyota', 2024, 'Corolla')
+myCar.getInfo();
+myCar.getModel();
+
+
+
+// ----------------------------
+
+
+function processValue(value: string | number): number {
+   if (typeof value === 'string') {
+      return value.length
+   } else {
+      return value * 2
+   }
+}
+
+
+// console.log(processValue(10));
+// console.log(processValue("hello"));
+
+
+// ----------------------------------------
+
+interface Product {
+   name: string;
+   price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+
+   if (products.length === 0) {
+      return null
+   }
+   return products.reduce((maxValue, currentValue) => {
+      return currentValue.price > maxValue.price  ? currentValue : maxValue
+   })
+}
+
+const products = [
+   { name: "Pen", price: 100 },
+   { name: "Notebook", price: 25 },
+   { name: "Bag", price: 50 }
+];
+
+console.log(getMostExpensiveProduct(products));
+
+
+
+
+
+
+
 
 
 
